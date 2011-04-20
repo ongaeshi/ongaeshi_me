fe82c344591c324982356de8518915c3d4a8dacc
ongaeshi <ongaeshi0621@gmail.com>
2011-04-21 02:41:41 +0900

    Add favicon, server.rb

---
+++ server.rb
@@ -0,0 +1,14 @@
+#! /opt/local/bin/ruby
+# -*- coding: utf-8 -*-
+
+require 'webrick'
+include WEBrick
+
+# ブラウザ開く
+system("open http://localhost:8005/")
+
+# webサーバー立ち上げ
+s = HTTPServer.new(:Port => 8005, :DocumentRoot => Dir::pwd)
+trap("INT"){ s.shutdown }
+s.start
+
